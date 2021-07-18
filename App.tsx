78e27e9ddb4157731ea50824dbd61f1e96205a4e
Easykatka <secondjump@bk.ru>
2021-07-18 23:02:49 +0300

    mobx+async fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import {Text, View} from "react-native";
+import * as Font from 'expo-font';
+import {observable} from "mobx";
+import {observer} from "mobx-react";
 
+@observer
+export default class App extends React.Component {
+	@observable fontLoaded = false;
 
-export default function App () {
-	return (
-		<View>
-			<Text>open up js</Text>
-		</View>
-	)
+	constructor(props: any) {
+		super(props)
+		this.init();
+	};
+
+	init = async () => {
+		await Font.loadAsync
+		({
+			'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+			'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+		});
+		this.fontLoaded = true;
+	};
+
+	render() {
+		return (
+			<View>
+				<Text>open up js</Text>
+			</View>
+		)
+	}
 };
+
+
+
+
